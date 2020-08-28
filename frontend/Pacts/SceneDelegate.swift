388e1911b8ddb72758575049f6267ff930e31ed8
Angela <angelachenchen19@gmail.com>
2020-08-28 15:19:26 -0400

    TabBarController
    Made a Main storyboard and a UITabBarController

--- frontend/Pacts/SceneDelegate.swift
+++ frontend/Pacts/SceneDelegate.swift
@@ -20,16 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
 
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
-    }
+
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -62,3 +54,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+}
