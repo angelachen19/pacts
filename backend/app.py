712c91109389139c8d423ae9eacc7b28f6cbb57b
Allison Hsu <allison.hsu20@gmail.com>
2020-08-28 12:13:25 -0400

    add rd db

---
+++ backend/app.py
@@ -0,0 +1,3 @@
+#create user
+#login user
+#get messages in a group
---
+++ backend/dao.py
@@ -0,0 +1,15 @@
+from time import time, ctime
+from db import db, User, Group, Activiity, Message, Poll, Event
+
+#User
+
+#Group
+
+#Activity
+
+#Message
+
+#Poll
+
+#Event
+
