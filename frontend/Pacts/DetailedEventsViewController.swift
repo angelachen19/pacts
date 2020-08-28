7a15437c45c79183c583049e5be0a91c2b9b5645
Angela <angelachenchen19@gmail.com>
2020-08-28 18:38:12 -0400

    Events Navigtation
    created a simple navigation for the events

---
+++ frontend/Pacts/DetailedEventsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailedEventsViewController.swift
+//  Pacts
+//
+//  Created by Angela Chen on 8/28/20.
+//  Copyright © 2020 Angela Chen. All rights reserved.
+//
+
+import UIKit
+
+class DetailedEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
